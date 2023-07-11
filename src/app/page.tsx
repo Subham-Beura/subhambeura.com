bbb0045bdfc6d6f00a1b9f1c77032add23b32b86
Subham Beura <subhambeura20@gmail.com>
2023-07-11 20:53:18 +0530

    add basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Button Trial</Button>
-    </main>
-  );
+  return <>Body</>;
 }
