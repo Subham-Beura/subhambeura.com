54fbefea3776b85dde99c76f19c6253c6adcbbf3
Subham Beura <subhambeura20@gmail.com>
2024-07-04 20:26:22 +0530

    styling: add hover effects to footer links

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -5,25 +5,24 @@ export default function Footer() {
   return (
     <div className="mt-[20vh]  md:mx-10 ">
       <div className="border-black border-t-4 h-[30vh] md:h-[40vh] font-sans px-4">
-        <p className="mx-2">Contact Me</p>
         <div className="font-sans font-bold  mt-10 ">
           <p>Get In Touch</p>
           <Link
             href="#"
-            className="hover:underline text-amber-600 my-10 w-full text-2xl h-fit break-words "
+            className="hover:underline text-amber-600 my-18 w-full text-2xl h-fit break-words "
           >
             subhambeura20@gmail.com
           </Link>
         </div>
       </div>
       <div className="border-black md:border-t-4 flex flex-col md:flex-row justify-between h-fit text-center">
-        <div className="pt-6 border-black md:w-[30vw] border-y-4 md:border-y-0 md:border-r-4 font-sans text-2xl font-semibold md:pt-10 h-24 ">
+        <div className="pt-6 border-black md:w-[30vw] border-y-4 md:border-y-0 md:border-r-4 font-sans text-2xl font-semibold md:pt-10 h-24 hover:bg-amber-500">
           Github
         </div>
-        <div className="pt-6 md:w-[30vw] font-sans  text-2xl font-semibold md:pt-10 h-24 ">
+        <div className="pt-6 md:w-[30vw] font-sans  text-2xl font-semibold md:pt-10 h-24 hover:bg-amber-500">
           LinkedIn
         </div>
-        <div className="pt-6 border-black border-t-4 md:border-t-0 md:border-l-4 md:w-[30vw] font-sans text-2xl font-semibold md:pt-10 h-24 ">
+        <div className="pt-6 border-black border-t-4 md:border-t-0 md:border-l-4 md:w-[30vw] font-sans text-2xl font-semibold md:pt-10 h-24 hover:bg-amber-500">
           Instagram
         </div>
       </div>
