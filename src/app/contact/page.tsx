621ba01a1d010da13197eee1ba17a92f95392a95
Subham Beura <subhambeura20@gmail.com>
2024-07-13 17:28:04 +0530

    feat: add links

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="text-center">
+      <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-4xl">
+        Contact Me
+      </h1>
+    </div>
+  );
 }
--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -16,15 +16,30 @@ export default function Footer() {
         </div>
       </div>
       <div className="border-black md:border-t-4 flex flex-col md:flex-row justify-between h-fit text-center">
-        <div className="pt-6 flex-col items-center justify-center border-black md:w-[33vw] border-y-4 md:border-y-0 md:border-r-4 font-sans text-2xl font-semibold md:pt-10 h-24 hover:bg-amber-500">
+        <Link
+          target="_blank"
+          rel="noopener noreferrer"
+          href={"https://github.com/Subham-Beura"}
+          className="pt-6 flex-col items-center justify-center border-black md:w-[33vw] border-y-4 md:border-y-0 md:border-r-4 font-sans text-2xl font-semibold md:pt-10 h-24 hover:bg-amber-500"
+        >
           <p>Github</p>
-        </div>
-        <div className="pt-6 md:w-[33vw] font-sans  text-2xl font-semibold md:pt-10 h-24 hover:bg-amber-500">
+        </Link>
+        <Link
+          className="pt-6 md:w-[33vw] font-sans  text-2xl font-semibold md:pt-10 h-24 hover:bg-amber-500"
+          target="_blank"
+          rel="noopener noreferrer"
+          href={"https://www.linkedin.com/in/subham-beura-aa5213231/"}
+        >
           LinkedIn
-        </div>
-        <div className="pt-6 border-black border-t-4 md:border-t-0 md:border-l-4 md:w-[33vw] font-sans text-2xl font-semibold md:pt-10 h-24 hover:bg-amber-500">
+        </Link>
+        <Link
+          target="_blank"
+          rel="noopener noreferrer"
+          href={"https://instagram.com/subham_beura15/?igsh=a3BpaXJpZmM4OGZr"}
+          className="pt-6 border-black border-t-4 md:border-t-0 md:border-l-4 md:w-[33vw] font-sans text-2xl font-semibold md:pt-10 h-24 hover:bg-amber-500"
+        >
           Instagram
-        </div>
+        </Link>
       </div>
     </div>
   );
